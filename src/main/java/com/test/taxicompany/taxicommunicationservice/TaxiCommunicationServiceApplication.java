881d468eae23304d82a730f611f1d0eafe22171b
jikkskv <jikkskv@gmail.com>
2024-07-21 20:12:51 +0800

    [intial-commit] Initial commit(project structure, mvn dependencies)

---
+++ src/main/java/com/test/taxicompany/taxicommunicationservice/TaxiCommunicationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.test.taxicompany.taxicommunicationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiCommunicationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiCommunicationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/taxicompany/taxicommunicationservice/TaxiCommunicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.taxicompany.taxicommunicationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiCommunicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
